36510809a62c79892733a6f48680d62bfe26aa40
MFDGaming <alexandros.argentakis@gmail.com>
2020-03-26 22:18:34 +0000

    Added FormAPI

--- src/creeperteam/profileui/ProfileUI.php
+++ src/creeperteam/profileui/ProfileUI.php
@@ -1,8 +1,41 @@
 namespace creeperteam\profileui;
 
+use creeperteam\form\CustomForm;
+use creeperteam\form\SimpleForm;
+use creeperteam\form\ModalForm;
 use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
 
 class ProfileUI extends PluginBase implements Listener {
+
+   /**
+     * @deprecated
+     *
+     * @param callable|null $function
+     * @return CustomForm
+     */
+    public function createCustomForm(?callable $function = null) : CustomForm {
+        return new CustomForm($function);
+    }
+
+    /**
+     * @deprecated
+     *
+     * @param callable|null $function
+     * @return SimpleForm
+     */
+    public function createSimpleForm(?callable $function = null) : SimpleForm {
+        return new SimpleForm($function);
+    }
+
+    /**
+     * @deprecated
+     *
+     * @param callable|null $function
+     * @return ModalForm
+     */
+    public function createModalForm(?callable $function = null) : ModalForm {
+        return new ModalForm($function);
+    }
 
 }
